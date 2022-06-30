57284419a7dc4691f78b65d2363e556dce965cb8
bosej2000 <59065256+bosej2000@users.noreply.github.com>
2022-06-30 13:00:05 +0530

    commit message by undefined

--- custom/tenant_4/4_Project1_samplePostExtension.js
+++ custom/tenant_4/4_Project1_samplePostExtension.js
@@ -1,15 +1,22 @@
-function execute_pre_query_extension(appContext,queryName,params)
+function execute_post_query_extension(appContext,queryName,params,data)
 {
-  console.log('I AM IN PRE-QUERY EXTENSION......'+ queryName);
+  console.log('I AM IN POST QUERY EXTENSION......*'+JSON.stringify(data)+'*');
+  return data;
 }
 
-function execute_pre_submit_extension(appContext,client,documentName,data)
+function execute_post_submit_extension(appContext,client,documentName,data)
 {
-  console.log('I AM IN PRE SUBMIT  EXTENSION......'+ documentName);
+  console.log('I AM IN POST SUBMIT  EXTENSION......'+ documentName);
+}
+
+function execute_validation_extension(appContext,client,documentName,data)
+{
+  console.log('I AM IN SUBMIT VALIDATION EXTENSION......'+ documentName);
 }
 
 module.exports = 
 {
-  execute_pre_query_extension : execute_pre_query_extension,
-  execute_pre_submit_extension : execute_pre_submit_extension
+  execute_post_query_extension : execute_post_query_extension,
+  execute_post_submit_extension: execute_post_submit_extension,
+  execute_validation_extension: execute_validation_extension
 }
